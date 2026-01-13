c06b6a184a82c74ffb3068a2ef1aa1d065f5964e
Felipe Padovani <felipepadovani@rubeus.com.br>
2026-01-13 16:21:33 -0300

    feat : sing-in page lg screens

--- app/components/Home/Logo.tsx
+++ app/components/Home/Logo.tsx
@@ -6,8 +6,8 @@ export default function Logo({grow}:{grow?:boolean}){
     <div className="flex items-center gap-2">
         <Check className="mb-1 bg-primary rounded p-1 text-white shadow-md" />
         <div>
-          <span className={cn("font-extrabold text-foreground", grow ? "text-2xl" : "text-xl")}>Goal</span>
-          <span className={cn("ml-1", grow ? "text-md" : "text-sm")}>TRACKER</span>
+          <span className={cn("font-extrabold text-foreground", grow ? "text-2xl lg:text-3xl" : "text-xl")}>Goal</span>
+          <span className={cn("ml-1", grow ? "text-md lg:text-2xl" : "text-sm")}>TRACKER</span>
         </div>
       </div>
   )
--- app/sign-in/[[...sing-in]]/page.tsx
+++ app/sign-in/[[...sing-in]]/page.tsx
@@ -10,7 +10,7 @@ export default async function SignInComponent() {
     <section className="display flex flex-col justify-start h-screen items-center gap-8 py-20">
       <div className="flex flex-col gap-2 justify-center items-center border-b border-b-gray-800/15 pb-8 mx-10">
         <Logo grow />
-        <span className="px-4 text-sm text-center text-foreground/70">
+        <span className="px-4 text-sm lg:text-md lg:w-96 text-center text-foreground/70">
           Track your goals, build better habits, and make progress every day.
         </span>
       </div>
