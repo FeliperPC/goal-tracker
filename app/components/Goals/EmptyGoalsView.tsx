0cb3f492b427d2d91aadb68fda995d5664ad896a
Felipe Padovani <felipepadovani@rubeus.com.br>
2026-01-14 11:22:29 -0300

    fix : emptyGOalsView on lg screens

--- app/components/Goals/EmptyGoalsView.tsx
+++ app/components/Goals/EmptyGoalsView.tsx
@@ -5,13 +5,13 @@ import Link from "next/link";
 export default function EmptyGoalsView() {
   return (
     <div className="flex flex-col border border-slate-300 px-4 py-6  bg-slate-100/50 rounded-xl shadow gap-3 items-center">
-      <InboxIcon className="size-10 text-gray-800" />
+      <InboxIcon className="size-10 text-gray-800 lg:size-12" />
       <div className="flex flex-col gap-1 items-center">
-        <h1 className="text-2xl text-gray-800">No goals yet</h1>
-        <p className="text-sm font-light text-gray-600">Start creating a goal.</p>
+        <h1 className="text-2xl text-gray-800 lg:text-3xl">No goals yet</h1>
+        <p className="text-sm font-light text-gray-600 lg:text-lg">Start creating a goal.</p>
       </div>
-      <ArrowDown className="text-gray-400 size-6" />
-      <Button asChild variant="link">
+      <ArrowDown className="text-gray-400 size-6 lg:size-8" />
+      <Button asChild variant="link" className="lg:text-lg">
         <Link href={"/dashboard/submit"}>
           New goal
         </Link>
